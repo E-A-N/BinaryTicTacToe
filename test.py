74adc112fe20de7304cf04008be08f0e79f13e5c
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-04 15:49:25 -0700

    implemented new test for matrix generator

--- test.py
+++ test.py
@@ -16,6 +16,23 @@ class binaryMatrixTest(unittest.TestCase):
         count = binTrix.bitCount(binAmount)
         statement = "This is the incorrect amound of bits."
         self.assertEqual(bits,count,statement)
+        
+    def test_matrix_data_one(self):
+        dataX = 0b010100000
+        dataY = 0b001000001
+        testData = binTrix.binCount(dataX,dataY,'x','y')
+        correctList = [" ","x","o","x"," "," "," "," ","o",]
+        statement = "This table is incorrect."
+        self.assertEqual(correctList,testData,statment)
+                         
+    def test_matrix_data_size(self):
+        dataX = 0b010100000
+        dataY = 0b001000001
+        testData = binTrix.binCount(dataX,dataY,'x','y')
+        testSize = len(testData)
+        correctSize = 9
+        statement = "This table is incorrect."
+        self.assertEqual(correctSize,testSize,statement)
 
 if __name__ == "__main__":
     unittest.main()
