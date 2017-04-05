073c3002bf402fd0eeae551e212619e4d3def623
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-04 17:00:43 -0700

    added more tests

--- test.py
+++ test.py
@@ -24,6 +24,30 @@ class binaryMatrixTest(unittest.TestCase):
         correctList = ['o', ' ', ' ', ' ', ' ', 'x', 'o', 'x', ' ']
         statement = "This table is incorrect."
         self.assertEqual(correctList,testData,statement)
+        
+    def test_matrix_data_two(self):
+        dataX = 0b111110000
+        dataY = 0b000001111
+        testData = binTrix.matrixData(dataX,dataY,'x','o')
+        correctList = ['x', 'x', 'x', 'x', 'x', 'o', 'o', 'o', 'o']
+        statement = "This table is incorrect."
+        self.assertEqual(correctList,testData,statement)
+        
+    def test_matrix_data_all_Xs(self):
+        dataX = 0b111111111
+        dataY = 0b000000000
+        testData = binTrix.matrixData(dataX,dataY,'x','o')
+        correctList = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
+        statement = "This table is incorrect."
+        self.assertEqual(correctList,testData,statement)
+        
+    def test_matrix_data_all_Ys(self):
+        dataX = 0b000000000
+        dataY = 0b111111111
+        testData = binTrix.matrixData(dataX,dataY,'x','o')
+        correctList = ['o', 'o', 'o', 'o', 'o','o','o','o','o']
+        statement = "This table is incorrect."
+        self.assertEqual(correctList,testData,statement)
                          
     def test_matrix_data_size(self):
         dataX = 0b010100000
