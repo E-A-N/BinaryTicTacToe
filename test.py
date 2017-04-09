a007c7a07beeeb476edc3c56185c93f5206f76e7
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-04-08 21:09:27 -0700

    added tests for vertical win checks

--- test.py
+++ test.py
@@ -148,6 +148,42 @@ class binaryMatrixTest(unittest.TestCase):
         result = binTrix.checkGameEnd(testGame,callBack,player) #should return a boolean
         statement = "The game should be over"
         self.assertTrue(result,statement)
+        
+    def test_win_mid_vert_x(self):
+        winConditionX = 0b010010010
+        player = "x"
+        testGame = ['', 'x', '', '', 'x', '', '', 'x', '']
+        callBack = binTrix.dataToBinary
+        result = binTrix.checkGameEnd(testGame,callBack,player) #should return a boolean
+        statement = "The game should be over"
+        self.assertTrue(result,statement)
+        
+    def test_win_mid_vert_o(self):
+        winConditionX = 0b010010010
+        player = "o"
+        testGame = ['', 'o', '', '', 'o', '', '', 'o', '']
+        callBack = binTrix.dataToBinary
+        result = binTrix.checkGameEnd(testGame,callBack,player) #should return a boolean
+        statement = "The game should be over"
+        self.assertTrue(result,statement)
+        
+    def test_win_right_vert_x(self):
+        winConditionX = 0b001001001
+        player = "x"
+        testGame = ['', '', 'x', '', '', 'x', '', '', 'x']
+        callBack = binTrix.dataToBinary
+        result = binTrix.checkGameEnd(testGame,callBack,player) #should return a boolean
+        statement = "The game should be over"
+        self.assertTrue(result,statement)
+        
+    def test_win_right_vert_o(self):
+        winConditionX = 0b001001001
+        player = "o"
+        testGame = ['', '', 'o', '', '', 'o', '', '', 'o']
+        callBack = binTrix.dataToBinary
+        result = binTrix.checkGameEnd(testGame,callBack,player) #should return a boolean
+        statement = "The game should be over"
+        self.assertTrue(result,statement)
 
 if __name__ == "__main__":
     unittest.main()
